805a5f7e7fa9dcc02b34e96bf86ff031b4c5ded5
chencoyote <chen_coyote@qq.com>
2014-11-07 22:27:17 +0800

    modify logo css and js

--- js/application.js
+++ js/application.js
@@ -3,13 +3,13 @@ var lucky = (function(){
 
     this.speed = 50;
     this.intervalID;
-    this.localdata;
+    this.localdata = "";
 
     this.luckyindex = 0;
     this.luckyname = "开始抽奖";
 
     this.init = function(data){
-        this.localdata = data
+        this.localdata = data;
         $("#lucky-name").text(this.luckyname);
 
         this._bindUI();
@@ -41,12 +41,12 @@ var lucky = (function(){
         $("#lucky-list").append('<span>'+luckyname+"</span>");
 
         this.localdata.splice(this.luckyindex, 1);
-    }
+    };
 
     this._bindUI = function(){
 
         // bind button
-        var trigger = document.querySelector('#go')
+        var trigger = document.querySelector('#go');
         trigger.innerHTML = trigger.getAttribute('data-text-start');
         trigger.addEventListener('click', go, false);
 
@@ -60,30 +60,35 @@ var lucky = (function(){
               trigger.setAttribute('data-action', 'start');
               trigger.innerHTML = trigger.getAttribute('data-text-start');
               stopRolling();
-            };
-        };
+            }
+        }
 
         // bind keydown
         document.addEventListener('keydown', function(ev) {
             if (ev.keyCode == '32') {
-              go()
+              go();
             }
             else if (ev.keyCode == '27') {
-              that.moveLucky()
+              that.moveLucky();
             }
         }, false);
     };
 
-    return this
+    return this;
 })();
 
 $(function(){
     lucky.init(data);
+    $("#diamond").unslider();
+    $("#platinum").unslider();
     $("#gold").unslider();
     $("#silver").unslider();
+    $("#bronze").unslider();
+    $("#partner").unslider();
+    
     
 
-})
+});
 /*$('#gold').carousel({
         showSurrounding: true 
         //是否显示周围的区域，不设置则默认为false，个别需要显示旁边元素显示美化效果的需求时用到，如百度相册首页就有这样的效果
