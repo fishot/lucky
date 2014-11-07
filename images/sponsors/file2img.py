49230d6e600f711508dee878b32ebcf4c0628d76
chencoyote <chen_coyote@qq.com>
2014-11-08 00:34:08 +0800

    remove unuse images

--- images/sponsors/file2img.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-files = os.listdir("./")
-patt = r"(\w+)_(\w+)_(\w+).(\w+)"
-for f in files:
-    res = re.match(patt, f)
-    if res:
-        print '<li><img src="./images/sponsors/%s" class="%s"></li>' % (res.group(0), res.group(3))
