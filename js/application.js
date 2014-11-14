e8515768a1f3baf27b55da6b6a45abb9370ac4fe
Todd Gao <todd.gao.2013@gmail.com>
2014-11-14 21:17:41 +0800

    clean dead code

--- js/application.js
+++ js/application.js
@@ -30,19 +30,16 @@ var lucky = (function(){
         $("#lucky-name").text(this.luckyname);
         this.bindUI();
     };
+
     this.rolling = function(){
-            var i = rand(this.localdata.length+1);
-            var name = this.localdata[i-1];
-
-            this.luckyindex = i-1;
-            this.luckyname = name;
-
-
-            $("#lucky-name").text(name);
+        var i = rand(this.localdata.length+1);
+        var name = this.localdata[i-1];
+        this.luckyindex = i-1;
+        this.luckyname = name;
+        $("#lucky-name").text(name);
     };
 
     this.startRolling = function(){
-        
         this.intervalID = setInterval(this.rolling, this.speed);
     };
 
@@ -80,36 +77,7 @@ var lucky = (function(){
                 go();
             }
         });
-
-    }
-
-    // this._bindUI = function(){
-    //     // bind button
-    //     var trigger = document.querySelector('#go');
-    //     trigger.innerHTML = trigger.getAttribute('data-text-start');
-    //     trigger.addEventListener('click', function(){
-    //         go();
-    //         $(this).blur();
-    //     }, false);
-    //     function go() {
-    //         if (trigger.getAttribute('data-action') === 'start') {
-    //           trigger.setAttribute('data-action', 'stop');
-    //           trigger.innerHTML = trigger.getAttribute('data-text-stop');
-    //           startRolling();
-    //         }
-    //         else {
-    //           trigger.setAttribute('data-action', 'start');
-    //           trigger.innerHTML = trigger.getAttribute('data-text-start');
-    //           stopRolling();
-    //         }
-    //     }
-    //     // bind keydown
-    //     document.addEventListener('keydown', function(ev) {
-    //         if (ev.keyCode == '32') {
-    //           go();
-    //         }
-    //     }, false);
-    // };
+    };
 
     return this;
 })();
@@ -124,31 +92,9 @@ $(function(){
 	$("#lucky-list").on("click" ,"span", function(){
 		var $self = $(this);
 		$(this).addClass("red")
-    //alert($(this).text());
-    //$(this).text = "-"+$(this).text();
-/*				.siblings().removeClass("red");*/
 	});
     $("#lucky-list").on("dblclick", "span", function(){
         var $self = $(this);
         $(this).removeClass("red");
     });
-    /*$("#diamond").unslider({
-		complete: function(){
-		}
-	});
-    $("#platinum").unslider();
-    $("#gold").unslider();
-    $("#silver").unslider();
-    $("#bronze").unslider();
-    $("#partner").unslider();*/
 });
-/*$('#gold').carousel({
-        showSurrounding: true 
-        //是否显示周围的区域，不设置则默认为false，个别需要显示旁边元素显示美化效果的需求时用到，如百度相册首页就有这样的效果
-        ,reverse:true //自动动画方向，不设置则默认为向右（false），设置为true则相反
-        ,interval: 1 //自动播放的间隔时间（秒），不设置则默认是5（秒）
-        ,speed: 0.2 //滚动一次需要的时间(秒)，不设置则默认是0.6（秒）
-        ,indicatorPosition: 'top' //指示器（小圆点）的位置（top，bottom），不设置则默认是bottom
-        ,indicatorAlign: 'right' //指示器水平位置（left，center，right），不设置则默认是center
-    });*/
-
